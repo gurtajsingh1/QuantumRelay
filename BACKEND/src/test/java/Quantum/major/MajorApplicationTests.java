351e8d9e76f44ae2005b933047fc7ff21ab5f4be
gurtajsingh1 <gurtajsinghhhh1@gmail.com>
2026-03-20 02:21:13 +0530

    .

---
+++ BACKEND/src/test/java/Quantum/major/MajorApplicationTests.java
@@ -0,0 +1,13 @@
+package Quantum.major;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MajorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
